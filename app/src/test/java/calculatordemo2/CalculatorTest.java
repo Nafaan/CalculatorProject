60757f0c0a69dbf2b27e54b2e9a9a9c922659192
Nathan Edwards <qbr1@txstate.edu>
2023-11-10 19:41:07 -0600

    Added 7 unit tests for twoOpOperations()

--- app/src/test/java/calculatordemo2/CalculatorTest.java
+++ app/src/test/java/calculatordemo2/CalculatorTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 class CalculatorTest {
     private static Calculator classUnderTest;
@@ -59,5 +61,113 @@ class CalculatorTest {
     @Test
     void testThrowError() {
         assertThrows(Error.class, () -> classUnderTest.calcScience(null, 10.0));
+    }
+
+    // Helper method to set private fields
+    private void setPrivateField(String fieldName, Object val) throws Exception {
+        Field field = Calculator.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(classUnderTest, val);
+    }
+    // Helper method to access private method
+    private double accessPrivateMethod(String methodName) throws Exception {
+        Method method = Calculator.class.getDeclaredMethod(methodName);
+        method.setAccessible(true);
+        return (double) method.invoke(classUnderTest);
+    }
+
+    @Test
+    public void testTwoOpAdd() throws Exception {
+        setPrivateField("num1", 9.0);
+        setPrivateField("num2", 30.0);
+        setPrivateField("mode", Calculator.twoOperator.add);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(39.0, result);
+    }
+
+    @Test
+    public void testTwoOpSub() throws Exception {
+        setPrivateField("num1", 30.0);
+        setPrivateField("num2", 9.0);
+        setPrivateField("mode", Calculator.twoOperator.subtract);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(21.0, result);
+    }
+
+    @Test
+    public void testTwoOpMult() throws Exception {
+        setPrivateField("num1", 9.0);
+        setPrivateField("num2", 3.0);
+        setPrivateField("mode", Calculator.twoOperator.multiply);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(27.0, result);
+    }
+    
+    @Test
+    public void testTwoOpDiv() throws Exception {
+        setPrivateField("num1", 9.0);
+        setPrivateField("num2", 3.0);
+        setPrivateField("mode", Calculator.twoOperator.divide);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(3.0, result);
+    }
+
+    @Test
+    public void testTwoOpMultAdd() throws Exception {
+        setPrivateField("num1", 9.0);
+        setPrivateField("num2", 3.0);
+        setPrivateField("mode", Calculator.twoOperator.multiply);
+
+        double initial = accessPrivateMethod("twoOpOperations");
+
+        setPrivateField("num1", initial);
+        setPrivateField("num2", 2.0);
+        setPrivateField("mode", Calculator.twoOperator.add);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(29.0, result);
+    }
+
+    @Test
+    public void testTwoOpDivAdd() throws Exception {
+        setPrivateField("num1", 9.0);
+        setPrivateField("num2", 3.0);
+        setPrivateField("mode", Calculator.twoOperator.divide);
+
+        double initial = accessPrivateMethod("twoOpOperations");
+
+        setPrivateField("num1", initial);
+        setPrivateField("num2", 21.0);
+        setPrivateField("mode", Calculator.twoOperator.add);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(24.0, result);
+    }
+
+    @Test
+    public void testTwoAddSub() throws Exception {
+        setPrivateField("num1", 9.0);
+        setPrivateField("num2", 3.0);
+        setPrivateField("mode", Calculator.twoOperator.add);
+
+        double initial = accessPrivateMethod("twoOpOperations");
+
+        setPrivateField("num1", initial);
+        setPrivateField("num2", 2.0);
+        setPrivateField("mode", Calculator.twoOperator.subtract);
+
+        double result = accessPrivateMethod("twoOpOperations");
+
+        assertEquals(10.0, result);
     }
 }
